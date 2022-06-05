37105e549eb24d19694ae32b075b31b1a2cb45df
Bob <bob.yuan@yahooo.com>
2022-06-05 12:11:51 -0700

    Setup initial Apollo server

---
+++ src/graphql/resolvers/greeting.resolver.ts
@@ -0,0 +1,7 @@
+export const resolvers = {
+    Query: {
+        greeting: () => {
+            return "Hello World from Apollo"
+        }
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,42 @@
-console.log('Hello world again....');
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { resolvers } from './graphql/resolvers/greeting.resolver';
+
+const main = async () => {
+
+    // Db connection
+
+
+
+    // Server setup
+    const app = express();
+
+    // REST api endpoint
+    app.get("/", (_, res) => {
+        res.send('Hello from EXPERSS');
+    });
+
+    const typeDefs = gql`
+        type Query {
+            greeting: String
+        }
+    `;
+
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    apolloServer.start().then(_ => {
+        apolloServer.applyMiddleware({ app });
+        
+        app.listen(4000, () => {
+            console.log('Apollo server started and listen to port: 4000');
+        })
+    });   
+
+}
+
+main().catch((err) => {
+    console.error(err);
+  });
