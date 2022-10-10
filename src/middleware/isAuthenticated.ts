8067a65b9a13a8962a1db400bfc05d322ea82931
Bob <bob.yuan@yahooo.com>
2022-10-09 18:05:59 -0700

    Further refactor context for authentication info

--- src/middleware/isAuthenticated.ts
+++ src/middleware/isAuthenticated.ts
@@ -3,27 +3,26 @@ import jwt from 'jsonwebtoken';
 const secret = process.env.JWT_SECRET;
 
 
-export const isAuthenticated = async (req:any, res:any, next:any) => {
+export const isAuthenticated = async (token: string) => {
     
-    let token:any;
     let decoded:any;
     // const authHeader = req.header.authorization; //req.get('Authorization');
 
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')
-      ) {
-        token = req.headers.authorization.split(' ')[1];
-      } 
-  
-      if (!token) return {authStatus: false};
+    if (!token) return null;
 
-      if(secret) {
-         decoded = jwt.verify(token, secret);
-      }
-     
+    // console.log(secret);
 
-      if (!decoded) return false;
+    if(secret) {
+      decoded = jwt.verify(token, secret);
+    }
+    // OR
+    // decoded = jwt.verify(token, secret!);   
 
-    return {authStatus: true, userId: decoded.userid};
+    if(!decoded) return null;
+    
+    console.log('verification', decoded);
+
+    return await decoded;
 
 
 }
